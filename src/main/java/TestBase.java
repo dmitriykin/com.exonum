7424844a76e2ae4bc0032035770a14a83eb9593c
Dmitriy <dmitriy29061990@gmail.com>
2019-06-05 16:48:13 +0300

    asd

--- src/main/java/TestBase.java
+++ src/main/java/TestBase.java
@@ -5,24 +5,26 @@ import java.util.concurrent.TimeUnit;
 
 public abstract class TestBase {
 
-    public WebDriver driver;
-    private String webSite = "https://exonum.com/demo/voting/#/elections/candidates";
+    private WebDriver driver = new ChromeDriver();
 
-    public String getWebSite() {
-        return webSite;
+    public WebDriver getDriver() {
+        return driver;
     }
+
+     public static final String WEB_SITE = "https://exonum.com/demo/voting/#/elections/candidates";
+
+
 
     // BeforeTest
     public void openPage() {
         System.setProperty("webdriver.chrome.driver", "C:\\driversForBrowsers\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.navigate().to(getWebSite());
+        driver.navigate().to(WEB_SITE);
         turnOnImplicitWait();
         driver.manage().window().maximize();
     }
 
     public void turnOnImplicitWait() {
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
     }
 
     public void turnOffImplicitWait() {
